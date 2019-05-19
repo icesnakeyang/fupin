61cc6224b1cb0f9813644059ad5dfb9ae09a465c
icesnakeyang <flow_ocean@yahoo.com>
2019-05-19 12:01:33 +0800

    progress

--- src/main/java/com/lito/fupin/controller/user/UserController.java
+++ src/main/java/com/lito/fupin/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.lito.fupin.controller.vo.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/admin/user")
 public class UserController {
     private final IUserBusinessService iUserBusinessService;
